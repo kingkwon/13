a13e2ec54357a5e039c6a3ea03ebcb600fe4a7ed
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-12-12 00:13:12 +0900

    실습 01

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	return 0;
+struct student{
+	int 	ID			;
+	char	name[20]	;
+	double	grade		;
+};
+void main(void) {
+	
+	struct student s = { 123, "KIM", 3.5};
+	
+	s.ID = 123456		;
+	s.name[0] = 'J'		;
+	s.name[1] = 'u'		;
+	s.name[2] = 'y'		;
+	s.name[3] = 'e'		;
+	s.name[4] = 'o'		;
+	s.name[5] = 'p'		;
+	s.grade = 4.2		;
+	
+	printf("ID : %d\n", s.ID)		;
+	printf("name : %s\n", s.name)	;
+	printf("grade : %f\n", s.grade)	;
+
 }
